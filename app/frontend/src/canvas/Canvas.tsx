ee7291e94de45892432b4f8bf9368dd09356ea03
Alexander James Phillips <ajp.global@gmail.com>
2020-12-22 20:00:40 +0000

    Implement zooming

--- app/frontend/src/canvas/Canvas.tsx
+++ app/frontend/src/canvas/Canvas.tsx
@@ -1,6 +1,10 @@
 import { h } from "preact"
 import { useState } from "preact/hooks"
 
+import { bounded } from "../utils"
+import "./Canvas.css"
+
+
 type MouseState =
 {
     down: false
@@ -26,7 +30,11 @@ export function Canvas ()
 {
     const [x, change_x] = useState(0)
     const [y, change_y] = useState(0)
-    const [scale, change_scale] = useState(1)
+    const [scale, _change_scale] = useState(1)
+    function change_scale (new_scale: number)
+    {
+        _change_scale(bounded(new_scale, 0.1, 4))
+    }
 
     function on_mouse_down (e: h.JSX.TargetedEvent<SVGSVGElement, MouseEvent>) {
         mouse_state.down = true
@@ -46,16 +54,27 @@ export function Canvas ()
         }
     }
 
+    function on_wheel (e: h.JSX.TargetedEvent<SVGSVGElement, WheelEvent>)
+    {
+        e.preventDefault()
+        change_scale(scale + (e.deltaY * -0.01 * scale))
+    }
+
     const transform = `translate(${x},${y}) scale(${scale})`
+    const background_style = {
+        backgroundPosition: `${x}px ${y}px`,
+        backgroundSize: `${20 * scale}px ${20 * scale}px`,
+    }
 
     return (
-    <div style={{ backgroundImage: "linear-gradient(to right, #4F4E4F 1px, transparent 1px),linear-gradient(to bottom, #4F4E4F 1px, transparent 1px)"}}>
+    <div id="paper_background" style={background_style}>
         <svg
             width="900"
             height="600"
             onMouseDown={on_mouse_down}
             onMouseMove={on_mouse_move}
             onMouseUp={on_mouse_up}
+            onWheel={on_wheel}
         >
             <g transform={transform}>
                 <rect width="100" height="100" />
--- app/frontend/src/utils.ts
+++ app/frontend/src/utils.ts
@@ -10,3 +10,9 @@ export function get_new_id ()
 {
     return Math.random().toString().slice(2)
 }
+
+
+export function bounded (num: number, min: number, max: number): number
+{
+    return Math.max(Math.min(num, max), min)
+}
