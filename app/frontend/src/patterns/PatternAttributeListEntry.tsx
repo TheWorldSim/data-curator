63ddece0e030c223fcdb41d4c5dbb80a529f7c95
Alexander James Phillips <ajp.global@gmail.com>
2020-12-27 18:22:46 +0000

    Add simple pattern attribute edit

--- app/frontend/src/patterns/PatternAttributeListEntry.tsx
+++ app/frontend/src/patterns/PatternAttributeListEntry.tsx
@@ -34,7 +34,7 @@ const map_state = (state: RootState, own_props: OwnProps): StateProps => {
 function _PatternAttributeListEntry (props: Props)
 {
     const item = props.id_map[props.attribute.type_id]
-    const desc = item ? description(item) : ""
+    const desc = item ? description(item) : props.attribute.type_id
 
     if (!props.editable)
     {
@@ -45,15 +45,48 @@ function _PatternAttributeListEntry (props: Props)
         ]
     }
 
+    function on_change_type_id (e: h.JSX.TargetedEvent<HTMLInputElement, Event>)
+    {
+        const type_id = e.currentTarget.value
+        if (props.editable) props.on_change({ ...props.attribute, type_id })
+    }
+
+    function on_change_alt_name (e: h.JSX.TargetedEvent<HTMLInputElement, Event>)
+    {
+        const alt_name = e.currentTarget.value
+        if (props.editable) props.on_change({ ...props.attribute, alt_name })
+    }
+
+    function on_change_multiple (e: h.JSX.TargetedEvent<HTMLInputElement, Event>)
+    {
+        const multiple = e.currentTarget.checked
+        if (props.editable) props.on_change({ ...props.attribute, multiple })
+    }
+
     return [
         <td>
-            <input type="text" placeholder="Statement Type or Pattern" value={desc}></input>
+            <input
+                type="text"
+                placeholder="Statement Type or Pattern"
+                value={desc}
+                onChange={on_change_type_id}
+            ></input>
         </td>,
         <td>
-            <input type="text" placeholder="Alternative description" value={props.attribute.alt_name}></input>
+            <input
+                type="text"
+                placeholder="Alternative description"
+                value={props.attribute.alt_name}
+                onChange={on_change_alt_name}
+            ></input>
         </td>,
         <td>
-            <input type="checkbox" title="Multiple values" checked={props.attribute.multiple}></input>
+            <input
+                type="checkbox"
+                title="Multiple values"
+                checked={props.attribute.multiple}
+                onChange={on_change_multiple}
+            ></input>
         </td>,
     ]
 }
