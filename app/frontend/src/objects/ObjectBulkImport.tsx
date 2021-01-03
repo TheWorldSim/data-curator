b60c59e7492f1297f20aa36804e013de67b8c8ef
Alexander James Phillips <ajp.global@gmail.com>
2021-01-03 13:59:12 +0000

    Convert temp airtable ids into full ids

--- app/frontend/src/objects/ObjectBulkImport.tsx
+++ app/frontend/src/objects/ObjectBulkImport.tsx
@@ -4,8 +4,9 @@ import { connect, ConnectedProps } from "react-redux"
 import { CORE_IDS } from "../state/core_data"
 
 import { merge_pattern_attributes } from "../state/objects"
-import type { ObjectWithCache, Pattern, RootState } from "../state/State"
+import { is_id_attribute, ObjectAttribute, ObjectWithCache, Pattern, RootState } from "../state/State"
 import { ACTIONS } from "../state/store"
+import { get_new_object_id } from "../utils/utils"
 
 
 interface OwnProps {}
@@ -80,7 +81,7 @@ function _ObjectBulkImport (props: Props)
 export const ObjectBulkImport = connector(_ObjectBulkImport) as FunctionComponent<OwnProps>
 
 
-const external_id_key = "airtable"
+const EXTERNAL_ID_KEY = "airtable"
 
 
 function get_data_from_air_table (pattern: Pattern, existing_objects: ObjectWithCache[], on_new_objects: (objects: ObjectWithCache[]) => void)
@@ -98,12 +99,14 @@ function get_data_from_air_table (pattern: Pattern, existing_objects: ObjectWith
     .then((d: { records: AirtableActions[] }) =>
     {
 
-        const objects = d.records.map(airtable_action =>
+        const objects_with_temp_ids = d.records.map(airtable_action =>
         {
             const predicate = find_object_by_airtable_id(airtable_action.id)
             const existing_object = existing_objects.find(predicate)
             return transform_airtable_action({ pattern, get_temp_id, airtable_action, existing_object })
         })
+
+        const objects = replace_temp_ids({ objects_with_temp_ids, existing_objects, temporary_ids })
 
         on_new_objects(objects)
     })
@@ -116,7 +119,7 @@ function find_object_by_airtable_id (airtable_id: string)
 
         if (!external_ids) return false
 
-        const id = external_ids[external_id_key]
+        const id = external_ids[EXTERNAL_ID_KEY]
         if (!id) return false
 
         return id === airtable_id
@@ -157,11 +160,11 @@ function transform_airtable_action (args: TransformAirtableActionArgs): ObjectWi
     const eo = args.existing_object
 
     return {
-        id: (eo && eo.id) || "",
+        id: (eo && eo.id) || get_new_object_id(),
         datetime_created: eo ? eo.datetime_created : new Date(aa.createdTime),
         labels: [],
         pattern_id: pattern.id,
-        external_ids: { [external_id_key]: aa.id, ...((eo && eo.external_ids) || {}) },
+        external_ids: { [EXTERNAL_ID_KEY]: aa.id, ...((eo && eo.external_ids) || {}) },
         pattern_name: pattern.name,
         content: pattern.content,
         attributes: merge_pattern_attributes([
@@ -172,7 +175,7 @@ function transform_airtable_action (args: TransformAirtableActionArgs): ObjectWi
             ...(aa.fields["Encompasing Action"] || []).map(id => ({
                 pidx: 4, id: args.get_temp_id(id),
             })),
-            { pidx: 5, value: args.get_temp_id((aa.fields["Depends on Actions"] || [])[0]) },
+            { pidx: 5, id: args.get_temp_id((aa.fields["Depends on Actions"] || [])[0]) },
         ], pattern),
         rendered: "",
         needs_rendering: true,
@@ -183,7 +186,7 @@ function transform_airtable_action (args: TransformAirtableActionArgs): ObjectWi
 interface TempIdFunc {
     (id: string | undefined): string
 }
-
+const TEMP_ID_PREFIX = "temp_id: "
 function temp_id_factory () {
     const temporary_ids: { [id: string]: number } = {}
 
@@ -193,11 +196,130 @@ function temp_id_factory () {
         if (!temporary_ids.hasOwnProperty(id)) temporary_ids[id] = 0
         temporary_ids[id] += 1
 
-        return `temp_id: ${id}`
+        return `${TEMP_ID_PREFIX}${id}`
     }
 
     return {
         temporary_ids,
         get_temp_id,
     }
+}
+
+
+interface ReplaceTempIdsArgs
+{
+    objects_with_temp_ids: ObjectWithCache[]
+    existing_objects: ObjectWithCache[]
+    temporary_ids: { [id: string]: number }
+}
+function replace_temp_ids (args: ReplaceTempIdsArgs): ObjectWithCache[]
+{
+    const { objects_with_temp_ids, existing_objects, temporary_ids } = args
+
+    const airtable_id_map = build_airtable_id_map({
+        objects_with_temp_ids,
+        existing_objects,
+        expected_ids: Object.keys(temporary_ids)
+    })
+
+    const objects = change_temp_ids({ objects_with_temp_ids, airtable_id_map })
+
+    return objects
+}
+
+
+interface BuildAirtableIdMapArgs
+{
+    objects_with_temp_ids: ObjectWithCache[]
+    existing_objects: ObjectWithCache[]
+    expected_ids: string[]
+}
+type ID_MAP = { [ airtable_id: string]: string }
+function build_airtable_id_map (args: BuildAirtableIdMapArgs): ID_MAP
+{
+    const { objects_with_temp_ids, existing_objects, expected_ids } = args
+
+    const id_map: ID_MAP = {}
+
+    mutate_id_map_with_objects(objects_with_temp_ids, id_map)
+    mutate_id_map_with_objects(existing_objects, id_map)
+
+    // Check all temporary_ids are present in map
+    const missing_ids: Set<string> = new Set()
+    expected_ids.forEach(id => {
+        if (!id_map.hasOwnProperty(id)) missing_ids.add(id)
+    })
+
+    if (missing_ids.size) throw new Error(`Missing ${missing_ids.size} ids from objects`)
+
+    return id_map
+}
+
+
+function mutate_id_map_with_objects (objects: ObjectWithCache[], id_map: ID_MAP)
+{
+    objects.forEach(o => {
+        const airtable_id = o.external_ids[EXTERNAL_ID_KEY]
+
+        if (!airtable_id) return
+
+        if (!o.id)
+        {
+            throw new Error(`No id for object`)
+        }
+
+        if (id_map.hasOwnProperty(airtable_id) && id_map[airtable_id] !== o.id)
+        {
+            throw new Error(`Mismatch in id map: ${airtable_id} -> ${o.id} but already have ${airtable_id} -> ${id_map[airtable_id]}`)
+        }
+
+        id_map[airtable_id] = o.id
+    })
+}
+
+
+interface ChangeTempIdsArgs
+{
+    objects_with_temp_ids: ObjectWithCache[]
+    airtable_id_map: ID_MAP
+}
+function change_temp_ids (args: ChangeTempIdsArgs)
+{
+    const { objects_with_temp_ids, airtable_id_map } = args
+
+    return objects_with_temp_ids.map(o => replace_attributes_temp_ids(o, airtable_id_map))
+}
+
+
+function replace_attributes_temp_ids (object: ObjectWithCache, airtable_id_map: ID_MAP): ObjectWithCache
+{
+    let changed = false
+
+    const new_attributes = object.attributes.map(a =>
+    {
+        const new_a = replace_attribute_temp_id(a, airtable_id_map)
+        const attribute_changed = new_a !== a
+        changed = changed || attribute_changed
+        return attribute_changed ? new_a : a
+    })
+
+    return changed ? { ...object, attributes: new_attributes } : object
+}
+
+
+function replace_attribute_temp_id (attribute: ObjectAttribute, airtable_id_map: ID_MAP): ObjectAttribute
+{
+    let new_id: string | undefined = undefined
+
+    if (is_id_attribute(attribute))
+    {
+        if (attribute.id.startsWith(TEMP_ID_PREFIX))
+        {
+            const temp_id = attribute.id.slice(TEMP_ID_PREFIX.length)
+            if (!airtable_id_map.hasOwnProperty(temp_id)) throw new Error(`Uknown temp_id: ${temp_id}`)
+            new_id = airtable_id_map[temp_id]
+        }
+    }
+
+    return new_id === undefined ? attribute : { ...attribute, id: new_id }
 }
