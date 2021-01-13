e646c1e87cd181291da98bc0e123d3acecf280bf
Alexander James Phillips <ajp.global@gmail.com>
2021-01-13 00:16:18 +0000

    Add a vertical fill colour for effort

--- app/frontend/src/canvas/Node.tsx
+++ app/frontend/src/canvas/Node.tsx
@@ -18,13 +18,29 @@ export function Node (props: OwnProps)
 
     const w = effort > 0 ? Math.max(width, 150) : 150
 
+    const style_outer: h.JSX.CSSProperties = {
+        width: w,
+        height,
+        left: x,
+        top: y,
+        display: display ? "" : "none",
+    }
+
+    const percent = `${Math.round(effort * 100)}%`
+    const backgroundImage = `linear-gradient(to top, #a6eaff ${percent}, rgba(0,0,0,0) ${percent})`
+    const style_inner: h.JSX.CSSProperties = {
+        padding: 5,
+        whiteSpace: "nowrap",
+        backgroundImage,
+    }
+
     return <div
         className={"graph_node " + (is_focused ? "focused" : "")}
-        style={{ width: w, height, left: x, top: y, display: display ? "" : "none" }}
+        style={style_outer}
         onMouseEnter={() => set_is_focused(true)}
         onMouseLeave={() => set_is_focused(false)}
     >
-        <div style={{ padding: 5, whiteSpace: "nowrap" }}>
+        <div style={style_inner}>
             {title}
 
             <hr />
