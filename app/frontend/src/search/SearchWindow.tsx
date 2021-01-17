5c3ee76ccc8b68f17e40392d5957879126d7f8dd
Alexander James Phillips <ajp.global@gmail.com>
2021-01-17 15:31:07 +0000

    Add modal and use for search window

--- app/frontend/src/search/SearchWindow.tsx
+++ app/frontend/src/search/SearchWindow.tsx
@@ -1,10 +1,10 @@
-import { FunctionComponent, h } from "preact"
-import { connect, ConnectedProps } from "react-redux"
+import { h } from "preact"
 
 import "./SearchWindow.css"
-import type { Item, RootState } from "../state/State"
+import type { Item } from "../state/State"
 import { ITEM_FILTERS, ListOfTypes } from "./ListOfTypes"
 import { useState } from "preact/hooks"
+import { Modal } from "../modal/Modal"
 
 
 interface OwnProps
@@ -16,18 +16,40 @@ interface OwnProps
 }
 
 
-function map_state (state: RootState)
+export function SearchWindow (props: OwnProps)
 {
-    return {
-        last_key: state.global_key_press.last_key,
-        last_key_time_stamp: state.global_key_press.last_key_time_stamp,
-    }
+    const [search_string, set_search_string] = useState("")
+
+    return <Modal
+        on_close={props.on_close}
+        title="Search"
+        child={child_props => {
+
+            const id_search_box = "search_box"
+            if (child_props.first_render) setTimeout(() => focus_search_box(id_search_box), 0)
+
+            return <div id="search_container">
+                <input
+                    id={id_search_box}
+                    type="text"
+                    value={search_string}
+                    onChange={e => set_search_string(e.currentTarget.value)}
+                ></input>
+
+                <br />
+                <br />
+                <hr />
+
+                <ListOfTypes
+                    specific_type_id={props.specific_type_id}
+                    filter_type={props.filter_type}
+                    filtered_by_string={search_string}
+                    on_click={(item: Item) => props.on_choose(item)}
+                />
+            </div>
+        }}
+    />
 }
-
-const connector = connect(map_state)
-type PropsFromRedux = ConnectedProps<typeof connector>
-
-type Props = PropsFromRedux & OwnProps
 
 
 function focus_search_box (html_id: string)
@@ -35,56 +57,3 @@ function focus_search_box (html_id: string)
     const el = document.getElementById(html_id)
     if (el) el.focus()
 }
-
-
-function calc_should_close (props: Props, time_stamp_first_rendered: number)
-{
-    const is_escape = props.last_key === "Escape"
-    const is_new = props.last_key_time_stamp && props.last_key_time_stamp > time_stamp_first_rendered
-
-    return is_escape && is_new
-}
-
-
-function _SearchWindow (props: Props)
-{
-
-    const time_stamp = performance.now()
-    const [time_stamp_first_rendered] = useState(time_stamp)
-    const [search_string, set_search_string] = useState("")
-    const first_render = time_stamp === time_stamp_first_rendered
-
-    const id_search_box = "search_box"
-    if (first_render) setTimeout(() => focus_search_box(id_search_box), 0)
-
-    const should_close = calc_should_close(props, time_stamp_first_rendered)
-    if (should_close) setTimeout(() => props.on_close(), 0)
-
-    return <div id="search_background">
-        <div id="search_container">
-            Search
-            <div id="search_close" onClick={() => props.on_close()}><span>X</span></div>
-
-            <input
-                id={id_search_box}
-                type="text"
-                value={search_string}
-                onChange={e => set_search_string(e.currentTarget.value)}
-            ></input>
-
-            <br />
-            <br />
-            <hr />
-
-            <ListOfTypes
-                specific_type_id={props.specific_type_id}
-                filter_type={props.filter_type}
-                filtered_by_string={search_string}
-                on_click={(item: Item) => props.on_choose(item)}
-            />
-        </div>
-    </div>
-}
-
-
-export const SearchWindow = connector(_SearchWindow) as FunctionComponent<OwnProps>
